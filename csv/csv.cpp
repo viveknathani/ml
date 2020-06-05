fa0ea8bd278d89d07d5b3036b114aebf3c6e9a73
viveknathani <viveknathani2402@gmail.com>
2020-06-05 15:37:24 +0530

    Function to get all rows

--- csv/csv.cpp
+++ csv/csv.cpp
@@ -3,6 +3,7 @@
 #include "csv.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -11,7 +12,30 @@ CSVReader::CSVReader(string f)
     this -> filename = f;
 }
 
-
 vector<vector<string>> CSVReader::getAllRows()
 {
+    fstream filePointer;
+    filePointer.open(this -> filename, ios::in);
+    
+    vector<vector<string>> rows;
+    vector<string> row;
+    string line;
+
+    while(!filePointer.eof())
+    {
+        filePointer >> line;
+
+        stringstream ss(line);
+
+        while(ss.good())
+        {
+            string columnData;
+            getline(ss, columnData, ',');
+            row.push_back(columnData);
+        }
+        rows.push_back(row);
+        row.clear();
+    }
+
+    return rows;
 }
